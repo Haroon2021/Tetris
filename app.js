c8931646d8a6f2ca87cfbc2ba8e2e80ed392fe1c
haroon <haroon@Dures-MacBook-Air.local>
2022-06-24 21:30:28 +0100

    Game mechanics all there except gameover

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const width = 10;
     let nextRandom = 0;
     let timerId ;    // This lets the timer Id be null
+    let score = 0;
 
     //The Tetrominoes
   const lTetromino = [
@@ -67,7 +68,7 @@ function undraw() {
 }
 
 //Make the tetrominos move down every second (or any other time interval)
-timerId = setInterval(moveDown,1000)
+// timerId = setInterval(moveDown,1000)
 
 // assign function to keycodes 
 // (keycodes help work out what button is being pressed): https://www.toptal.com/developers/keycode/for/arrow-up
@@ -104,6 +105,7 @@ function freeze() {
     currentPosition = 4
     draw()
     displayShape()
+    addScore();
   }
 }
 
@@ -184,6 +186,25 @@ startBtn.addEventListener('click', () => {
     displayShape()
   }
 })
+
+// Add Score function
+function addScore() {
+  for (let i=0; i<199; i += width) {
+    const row = [i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,i+8,i+9]
+    
+    if(row.every(index => squares[index].classList.contains('taken'))) {
+      score += 10;
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+        squares[index].classList.remove('taken')
+        squares[index].classList.remove('tetromino')
+      })
+      const squaresRemoved = squares.splice(i, width)
+      squares = squaresRemoved.concat(squares)
+      squares.forEach(cell => grid.appendChild(cell))
+    }
+  }
+}
 
 
 
